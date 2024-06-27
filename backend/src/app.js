04efd82876420fe257e07d686006b2543ac19c95
Ricardo Martínez <theilluminatiduck@gmail.com>
2024-06-26 21:34:10 -0700

    asdas

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,10 +28,10 @@ app.use(
 app.use("/v1", apiV1)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
   });
 }
 
